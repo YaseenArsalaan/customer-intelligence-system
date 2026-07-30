805a01b34dada0d128712e725a343a4cd7abaec7
Yaseen Arsalaan <76151529+YaseenArsalaan@users.noreply.github.com>
2026-07-31 03:24:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(page_title="🧠 YAXΞ Customer Intelligence", layout="wide")
 # =========================
 # TITLE
 # =========================
-st.title("YAXΞ Customer Intelligence")
+st.title("🧠 YAXΞ Customer Intelligence")
 
 # =========================
 # LOAD DATA
